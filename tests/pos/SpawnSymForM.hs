6e53e6f15f06c87f91385aafa86ec1ef391b04fc
Alexander Bakst <abakst@cs.ucsd.edu>
2017-03-22 11:48:52 -0700

    forM

---
+++ tests/pos/SpawnSymForM.hs
@@ -0,0 +1,40 @@
+{- OPTIONS_GHC -fplugin Brisk.Plugin #-}
+{- OPTIONS_GHC -fplugin-opt Brisk.Plugin:main #-}
+{-# LANGUAGE TemplateHaskell #-}
+module SpawnSym where
+
+import Control.Monad (forM, foldM)
+import Control.Distributed.Process
+import Control.Distributed.BriskStatic
+import Control.Distributed.Process.Closure
+import Control.Distributed.Process.SymmetricProcess
+
+import GHC.Base.Brisk
+
+p :: ProcessId -> Process ()
+p who = do self <- getSelfPid
+           c <- liftIO $ getChar
+           msg <- if c == 'x' then return (0 :: Int) else return 1
+           send who msg
+           expect :: Process ()
+           return ()
+
+remotable ['p]
+
+ack :: ProcessId -> Process ()
+ack p = send p ()
+
+broadCast :: SymSet ProcessId -> Process ()
+broadCast pids
+  = do forM pids go
+       forM pids go'
+       return ()
+         where
+           go  p = expect :: Process Int
+           go'   = ack
+
+main :: [NodeId] -> Process ()
+main nodes = do me     <- getSelfPid
+                symSet <- spawnSymmetric nodes $ $(mkBriskClosure 'p) me
+                broadCast symSet
+                return ()
